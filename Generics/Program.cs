56c08009824a6756bd374929cddc1c9d76fd136c
derya ünver <yl1930138003@ogr.sdu.edu.tr>
2020-12-07 16:34:42 +0300

    Generic sınıflar,Generic Metodlar,Generic Kısıtlar

---
+++ Generics/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Utilities utilities = new Utilities();
+            List<string> result = utilities.BuildList<string>("Ankara", "İzmir", "İstanbul");
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+
+            List<Customer> result2 = utilities.BuildList<Customer>(
+                new Customer
+            {
+               Firstname = "Derya"
+            }, new Customer
+            {
+                Firstname = "Emrah"
+            });
+            foreach (var customer in result2)
+            {
+                Console.WriteLine(customer.Firstname);
+            }
+            Console.ReadKey();
+        }
+      
+    }
+    class Utilities
+    {
+        public List<T> BuildList<T>(params T[] items)
+        {
+            return new List<T>(items);
+        }
+    }
+
+
+    class Customer:IEntity
+    {
+        public  string Firstname { get; set; }
+    }
+    interface ICustomerDal : IRepository<Customer>
+    {
+        void Custom();
+    }
+    class CustomerDal : ICustomerDal
+    {
+        public void Add(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Custom()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Customer> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    interface IEntity
+    {
+        
+    }
+
+
+
+    class Product:IEntity
+    {
+
+    }
+    interface IProductDal : IRepository<Product>
+    {
+
+    }
+    interface IRepository<T> where  T: class,IEntity,new()
+    {
+        List<T> GetAll();
+        T Get(int id);
+        void Add(T entity);
+        void Update(T entity);
+
+        void Delete(T entity);
+    }
+    class ProductDal : IProductDal
+    {
+        public void Add(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
