3535e3e5769a80e069a751bbdb47570c2f42c454
derya ünver <yl1930138003@ogr.sdu.edu.tr>
2020-12-09 22:39:26 +0300

    Activator.CreateInstance

--- Reflection/Program.cs
+++ Reflection/Program.cs
@@ -10,6 +10,50 @@ namespace Reflection
     {
         static void Main(string[] args)
         {
+          //DortIslem dortIslem= new DortIslem(2,3);
+          //Console.WriteLine(dortIslem.Topla2());
+          //Console.WriteLine(dortIslem.Topla(3,4));
+
+          var tip = typeof(DortIslem);
+          DortIslem dortIslem=(DortIslem)Activator.CreateInstance(tip,6,7);
+          Console.WriteLine(dortIslem.Topla(2,3));
+          Console.WriteLine(dortIslem.Topla2());
+
+
+          Console.ReadKey();
+
+        }
+    }
+
+    public class DortIslem
+    {
+        private int _sayi1;
+        private int _sayi2;
+        public DortIslem(int sayi1, int sayi2)
+        {
+            _sayi1 = sayi1;
+            _sayi2 = sayi2;
+        }
+
+
+        public int Topla(int sayi1, int sayi2)
+        {
+            return sayi1 +sayi2;
+        }
+
+
+        public int Carp(int sayi1, int sayi2)
+        {
+            return sayi1 * sayi2;
+        }
+
+        public int Topla2()
+        {
+            return _sayi1 + _sayi2;
+        }
+        public int Carp2()
+        {
+            return _sayi1 * _sayi2;
         }
     }
 }
