e1f086d0e80baf29a52c61976ad6892bb0056ba8
derya ünver <yl1930138003@ogr.sdu.edu.tr>
2020-12-12 16:50:29 +0300

    Event kullanımı

---
+++ Events/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product harddisk = new Product(50);
+            harddisk.ProductName = "HARD DİSK";
+            Product gsm = new Product(50);
+            gsm.ProductName = "GSM";
+            gsm.StockControlEvent += Gsm_StockControlEvent;
+            for (int i = 10; i <=gsm.Stock; i++)
+            {
+                harddisk.Sell(10);
+                gsm.Sell(10);
+                Console.ReadLine();
+            }
+
+            //Console.ReadKey();
+
+        }
+
+        private static void Gsm_StockControlEvent()
+        {
+            Console.WriteLine("GSM about to finished!!!");
+        }
+    }
+}
